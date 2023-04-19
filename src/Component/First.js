93c065c8d9a08e20bb4c6cd80d5a74b058f7acf7
priyankapsg <priyaprem1801@gmail.com>
2023-04-19 18:46:55 +0530

    commit

---
+++ src/Component/First.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to my homepage!</h1>
+      <button>BUYER</button>
+      <button>SELLER</button>
+      <button>RENTAL</button>
+    </div>
+  );
+}
+
+export default HomePage;
