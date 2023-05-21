4436e6492d36bd11c00aa9fa5d25cc824b83be95
priyankapsg <priyaprem1801@gmail.com>
2023-05-21 19:55:18 +0530

    bg

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -15,24 +15,42 @@ navigate(`./flats/${type}`)
 }
 
   return (
-     <div className='header'>
-      <div className='animate-charcter' style={{color:"ButtonHighlight",fontFamily:"Lucida Handwriting",padding: "200px 110px 10px 504px"}}> <h1 color='blue'>WELCOME TO DREAM HOME</h1></div>
+    
+     <div>
+      <nav>
+    <div class="menu">
+      <div class="logo">
+        <a href="#">Elite Estates</a>
+      </div>
+      {/* <div className='animate-charcter' style={{color:"ButtonHighlight",fontFamily:"Lucida Handwriting",padding: "200px 110px 10px 504px"}}> <h1 color='blue'>WELCOME TO DREAM HOME</h1></div>
 
 <nav className="navbar navbar-expand-lg navbar-light" style={{padding: "5px 110px 330px 594px"}}>
   <div className="collapse navbar-collapse" id="navbarText">
-    <ul className="navbar-nav mr-auto" style={{gap:'20px'}}>
-      <li className="nav-item active">
-      <button       onClick={() => handleFlat('sale')} className="btn btn-primary ">Buy</button>
+    <ul className="navbar-nav mr-auto" style={{gap:'20px'}}> */}
+ <ul>
+      <li>
+      <a href='#'onClick={() => handleFlat('sale')}>buy</a>
       </li>
-      <li className="nav-item">
-            <button onClick={() => handleFlat('rent')} className="btn btn-primary ">Rent</button>
+      <li>
+            <a href="#" onClick={() => handleFlat('rent')} >Rent</a>
       </li>
-      <li className="nav-item">
-            <button onClick={handleRoute} className="btn btn-primary ">Post your Property</button>
+      <li>
+            <a href="#" onClick={handleRoute} >Post your Property</a>
       </li>
     </ul>
   </div>
 </nav>
+ 
+  <div class="img"></div>
+  <div class="center">
+    <div class="title">Sell your home, open new doors</div>
+    <div class="sub_title"> Our app makes it effortless</div>
+    <div class="btns">
+      <button>SEARCH</button>
+      {/* <button>Subscribe</button> */}
+    </div>
+  </div>
+
 {/* <body>
 <div class="waviy">
    <span style="--i:1">A</span><br/>
@@ -55,8 +73,7 @@ navigate(`./flats/${type}`)
    <span style="--i:19">.</span>
   </div>
   </body> */}
-    </div>
-     
+  </div>
   );
 }
 
